7cd68fbf74ea8c67afd14d36597546fa399a878c
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-09 14:55:05 +0530

    Delete configuration_limits.py

--- configuration_limits.py
+++
@@ -1,4 +0,0 @@
-limits = {'PASSIVE_COOLING':{'lowerLimit':0,'upperLimit':35},
-          'HI_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':45},
-          'MED_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':40}
-         }
