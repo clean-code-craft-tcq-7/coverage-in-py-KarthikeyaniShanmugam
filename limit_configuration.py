dfa7825433c6c854672b6afd7c7be14d0e5b3075
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-09 10:53:34 +0530

    Add files via upload

---
+++ limit_configuration.py
@@ -0,0 +1,4 @@
+limits = {'PASSIVE_COOLING':{'lowerLimit':0,'upperLimit':35},
+          'HI_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':45},
+          'MED_ACTIVE_COOLING':{'lowerLimit':0,'upperLimit':40}
+         }
