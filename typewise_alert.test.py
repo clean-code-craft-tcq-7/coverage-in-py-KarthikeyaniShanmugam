3cdf3d01a05eede5730443ba6ca3f2060309dac2
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-10 15:52:38 +0530

    Update typewise_alert.test.py

--- typewise_alert.test.py
+++ typewise_alert.test.py
@@ -101,13 +101,13 @@ class TypewiseTest(unittest.TestCase):
     self.assertTrue(infer_breach(1.2, 0.1, 1.0) == 'TOO_HIGH')
     self.assertTrue(infer_breach(0.02, 0.1, 1.0) == 'TOO_LOW')
 
-#   def test_sender(self):
-#     self.assertTrue(send_to_controller('TOO_HIGH') == True) 
-#     self.assertTrue(send_to_controller('TOO_LOW') == True) 
-#     self.assertTrue(send_to_controller('NORMAL') == True) 
+  def test_sender(self):
+    self.assertTrue(send_to_controller('TOO_HIGH') == True) 
+    self.assertTrue(send_to_controller('TOO_LOW') == True) 
+    self.assertTrue(send_to_controller('NORMAL') == True) 
 
-#     self.assertTrue(send_to_email('TOO_HIGH') == True) 
-#     self.assertTrue(send_to_email('TOO_LOW') == True) 
-#     self.assertTrue(send_to_email('NORMAL') == False) 
+    self.assertTrue(send_to_email('TOO_HIGH') == True) 
+    self.assertTrue(send_to_email('TOO_LOW') == True) 
+    self.assertTrue(send_to_email('NORMAL') == False) 
 if __name__ == '__main__':
   unittest.main()
