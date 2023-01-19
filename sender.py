7b2bbf7dd84e89a0e74faae97e479e0823548149
Karthikeyani Shanmugam <41341729+KarthikeyaniShanmugam@users.noreply.github.com>
2023-01-19 10:29:27 +0530

    Update sender.py

--- sender.py
+++ sender.py
@@ -3,13 +3,12 @@ from printer import *
 
 def send_to_controller(breachType):
   print_send_controller(header,breachType)
-  return True
+
 
 def send_to_email(breachType):  
   if breachType == 'TOO_LOW':
     print_send_email(recepient,'too low')
-    return True
+
   elif breachType == 'TOO_HIGH':
     print_send_email(recepient,'too high')
-    return True
-  return False
+
